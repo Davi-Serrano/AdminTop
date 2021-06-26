8dfc9af050aa7d75daaaafee7008a955aac2f749
Davi Serrano <daviscardoso2901@gmail.com>
2021-06-26 16:10:37 -0300

    Organizando cod, add coments e formatação

--- router/usuario.js
+++ router/usuario.js
@@ -2,85 +2,94 @@ const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/credito")
-const Credito= mongoose.model("credito")
+const Credito = mongoose.model("credito")
 require("../models/debito")
-const Debito= mongoose.model("debito")
+const Debito = mongoose.model("debito")
 
 
-
-
-
-
+//Faz o cadastro de crédito e débito
 router.get("/", (req, res) => {
 
     res.render("../views/index");
 })
 
-router.post("/show", (req, res)=>{
+//Posta Crédito 
+router.post("/saveC", (req, res) => {
 
-        const newCred = {
-            Descricao: req.body.descricaoCred,
-            Valor: req.body.valorCred
-        }
-        new Credito(newCred).save().then(()=>{
-            console.log("Salvo com sucesso")
-            res.redirect("/ler")
-        }).catch((err) =>{
-            console.log(err)
-        })
+    const newCred = {
+        Descricao: req.body.descricaoCred,
+        Valor: req.body.valorCred
+    }
+    new Credito(newCred).save().then(() => {
+        console.log("Salvo com sucesso")
+        res.redirect("/balanco")
+    }).catch((err) => {
+        console.log(err)
     })
-
-router.post("/save", (req, res) => {
-        
-    const newDeb = {
-            Descricao: req.body.descricaoDeb,
-            Valor: req.body.valorDeb
-        }
-        new Debito(newDeb).save().then(()=>{
-            console.log("Salvo com sucesso")
-            res.redirect("/ler")
-        }).catch((err) =>{
-            console.log(err)
-        })
-    
 })
 
-router.get('/ler',  (req, res) => {
- 
+//Posta Débito
+router.post("/saveD", (req, res) => {
 
-    Debito.find().lean().then((debito) =>{
-    
-        Credito.find().lean().then((credito) =>{
-        
-            res.render("../views/ler", {credito: credito, debito:debito})
-        }).catch((err) =>{
+    const newDeb = {
+        Descricao: req.body.descricaoDeb,
+        Valor: req.body.valorDeb
+    }
+    new Debito(newDeb).save().then(() => {
+        console.log("Salvo com sucesso")
+        res.redirect("/balanco")
+    }).catch((err) => {
+        console.log(err)
+    })
+
+})
+
+//Pag de visualização do balanço
+router.get('/balanco', (req, res) => {
+
+
+    Debito.find().lean().then((debito) => {
+
+        Credito.find().lean().then((credito) => {
+
+            res.render("../views/balanco", {
+                credito: credito,
+                debito: debito
+            })
+        }).catch((err) => {
             console.log(err)
-                }).catch((err) =>{
-                    console.log(err)
-                    })
-            
-             })
-
-    })
-
-    router.post("/del", (req, res) =>{
-        
-        Credito.deleteOne({_id: req.body.id}).then(() => {
-            res.redirect("/ler")
-        }).catch((err) =>{
-            res.redirect("/ler")
+        }).catch((err) => {
+            console.log(err)
         })
+
     })
-    
-    router.post("/deldeb", (req, res) =>{
-            
-        Debito.deleteOne({_id: req.body.id}).then(() => {
-                        res.redirect("/ler")
-                    }).catch((err) =>{
-                        res.redirect("/ler")
-                    })
-                })
-    
+
+})
+
+//Deleta Crédito
+router.post("/delC", (req, res) => {
+
+    Credito.deleteOne({
+        _id: req.body.id
+    }).then(() => {
+        res.redirect("/balanco")
+    }).catch((err) => {
+        res.redirect("/balanco")
+    })
+})
+
+//Deleta Débito
+router.post("/delB", (req, res) => {
+
+    Debito.deleteOne({
+        _id: req.body.id
+    }).then(() => {
+        res.redirect("/balanco")
+    }).catch((err) => {
+        res.redirect("/balanco")
+    })
+})
+
 
 
 
